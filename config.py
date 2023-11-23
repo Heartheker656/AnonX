2aab42f597f71ae445adb8e0a3a26ce8f093bbc4
dtroxD <151438386+dtroxD@users.noreply.github.com>
2023-11-23 23:12:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
-
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/dtroxD/AnonX")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/dtroxD/AnonX")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
