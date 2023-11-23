19c701a79354b39fd81c3d0459d89ed5dc09f8d5
dtroxD <151438386+dtroxD@users.noreply.github.com>
2023-11-24 00:45:11 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -6,30 +6,40 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
+import math
+from AnonX.utils.formatters import time_to_seconds
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-import random
-
-from pyrogram.types import InlineKeyboardButton
-
-selections = [
-    "◉—————————",    
-    "—◉————————",    
-    "——◉———————",
-    "———◉——————",
-    "————◉—————",
-    "—————◉————",
-    "——————◉———",
-    "———————◉——",
-    "————————◉—",
-    "—————————◉",
-]
 
 
 ## After Edits with Timer Bar
 
-
 def stream_markup_timer(_, videoid, chat_id, played, dur):
-    bar = random.choice(selections)
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    anon = math.floor(percentage)
+    if 0 < anon <= 10:
+        bar = "◉—————————"
+    elif 10 < anon < 20:
+        bar = "—◉————————"
+    elif 20 <= anon < 30:
+        bar = "——◉———————"
+    elif 30 <= anon < 40:
+        bar = "———◉——————"
+    elif 40 <= anon < 50:
+        bar = "————◉—————"
+    elif 50 <= anon < 60:
+        bar = "—————◉————"
+    elif 60 <= anon < 70:
+        bar = "——————◉———"
+    elif 70 <= anon < 80:
+        bar = "———————◉——"
+    elif 80 <= anon < 95:
+        bar = "————————◉—"
+    else:
+        bar = "—————————◉"
+
     buttons = [
         [
             InlineKeyboardButton(
