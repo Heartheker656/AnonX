30f7e581b06b0a09b5ef619eb3ec48b6f0b748da
dtroxD <151438386+dtroxD@users.noreply.github.com>
2023-11-24 13:10:52 +0530

    Update maintenance.py

--- AnonX/plugins/maintenance.py
+++ AnonX/plugins/maintenance.py
@@ -9,6 +9,7 @@ from AnonX.utils.database import (get_lang, is_maintenance,
                                        maintenance_on)
 from AnonX.utils.decorators.language import language
 
+
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
 
@@ -29,7 +30,7 @@ async def maintenance(client, message: Message):
     if state == "enable":
         if await is_maintenance() is False:
             await message.reply_text(
-                "ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ."
+                "Maintenance mode is already enabled"
             )
         else:
             await maintenance_on()
@@ -40,7 +41,7 @@ async def maintenance(client, message: Message):
             await message.reply_text(_["maint_3"])
         else:
             await message.reply_text(
-                "ɪ ᴅᴏɴ'ᴛ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ᴇɴᴀʙʟᴇᴅ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ."
+                "Maintenance mode is already disabled"
             )
     else:
         await message.reply_text(usage)
